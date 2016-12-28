ab8b557e48f94cf5ee5f44009ca5de5c333f6b97
unknown <damilola.durodola@ymail.com>
2016-12-28 11:51:54 +0100

    Made some modifications

--- try.py
+++ try.py
@@ -2,11 +2,13 @@ import re
 import nltk
 import pandas as pd
 from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.decomposition import TruncatedSVD
 from sklearn.pipeline import make_pipeline
-from sklearn.externals import joblib
+from sklearn.preprocessing import  Normalizer
+from sklearn.cross_validation import train_test_split
 from sklearn import metrics
 from time import time
 
@@ -36,44 +38,13 @@ def tokenize_only(text):
             filtered_tokens.append(token)
     return filtered_tokens
 
-def feature_extraction_classify(words):
 
-    t0 = time()
-    tfidf = TfidfVectorizer(stop_words='english', max_df=0.5, max_features=1000, min_df=2, tokenizer=tokenize_only,
-                            ngram_range=(1, 1), use_idf=True)
-    tfs = tfidf.fit_transform(X_train.values.astype('U'))
-    print("done in %fs" % (time() - t0))
-    # term = tfidf.get_feature_names()
-    # print(term)
-    # print (tfs.shape)
-
-    # Classify
-    nb = MultinomialNB().fit(tfs, y_train)
-
-    # print(nb)
-
-    #Make predictions
-    pred_test = tfidf.transform(X_test.values.astype('U'))
-    predicted = nb.predict(pred_test)
-
-    # for doc, category in zip(X_test, predicted):
-    #     print('%r => %s' % (doc, y_test))
-
-    # saving trained model
-    # joblib.dump(nb, 'nb_classify.pkl')
-
-    # raising valueerror
-    print(nb.score(X_train, y_train))
-    print(nb.score(X_test, y_test))
-    # calculate accuracy
-    print(metrics.accuracy_score(y_test, predicted))
-    # confusion matrix
-    print(metrics.confusion_matrix(y_test, predicted))
 
 
 #Read the tweets one by one and process it
 
-inpTweets = pd.read_excel(r"Book1.xlsx")
+inpTweets = pd.read_excel(r"C:\Users\Dharmie\PycharmProjects\sentAnalysis\Book1.xlsx")
+inpTweets.shape
 X = inpTweets['Tweets']
 y = inpTweets['cluster']
 
@@ -83,4 +54,22 @@ print(X_test.shape)
 print(y_train.shape)
 print(y_test.shape)
 
-feature_extraction_classify(inpTweets['Tweets'])
+vect = CountVectorizer()
+hi = vect.fit_transform(X_train)
+print(hi)
+
+nb = MultinomialNB()
+nb.fit(hi, y_train)
+# make prediction
+y_pred_classs = nb.predict(hi)
+# calculate accuracy
+metrics.accuracy_score(y_test, y_pred_classs)
+# confusion matrix
+metrics.confusion_matrix(y_test, y_pred_classs)
+# transform data to a document-term matrix
+# dami = vect.transform(words)
+# print(dami)
+# convert sparse matrix to dense matrix
+# dami.toarray()
+# examine vocabulary and document-term matrix together
+# pd.DataFrame(dami.toarray(), columns=vect.get_feature_names())
